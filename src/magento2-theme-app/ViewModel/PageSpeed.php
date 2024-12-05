b71456c5164ea05919e55c36ce35547d3c7be4cc
Server <server@fashionwallpaper.co.uk>
2024-12-05 16:03:21 +0000

    1.0.14

--- src/magento2-theme-app/ViewModel/PageSpeed.php
+++ src/magento2-theme-app/ViewModel/PageSpeed.php
@@ -7,12 +7,17 @@ namespace FishPig\ThemeApp\ViewModel;
 class PageSpeed implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     /**
+     * 
+     */
+    private $objectManager;
+
+    /**
      *
      */
     public function __construct(
         private \Magento\Framework\Module\Manager $moduleManager
     ) {
-
+        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
     }
 
     /**
@@ -50,6 +55,18 @@ class PageSpeed implements \Magento\Framework\View\Element\Block\ArgumentInterfa
     /**
      *
      */
+    public function isEnabledMinifyCss(): bool
+    {
+        if (!$this->isEnabled()) {
+            return false;
+        }
+
+        return $this->getCssAssetHelper()->isEnabledMinify();
+    }
+
+    /**
+     *
+     */
     public function getDataHelper()
     {
         return $this->objectManager->get(\FishPig\PageSpeed\Helper\Data::class);
@@ -61,5 +78,14 @@ class PageSpeed implements \Magento\Framework\View\Element\Block\ArgumentInterfa
     public function getJsAssetHelper()
     {
         return $this->objectManager->get(\FishPig\PageSpeed\Model\AssetHelper\Javascript::class);
+    }
+
+
+    /**
+     *
+     */
+    public function getCssAssetHelper()
+    {
+        return $this->objectManager->get(\FishPig\PageSpeed\Model\AssetHelper\Css::class);
     }
 }
